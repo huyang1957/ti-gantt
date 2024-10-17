16f37e97bb5de1a4528abb4b11eacc3de660fba1
huyang1957 <huyang1957@163.com>
2024-10-17 11:07:53 +0800

    完整的目录结构

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import TiBtn from "./components/TiBtn.vue"
+</script>
+
+<template>
+  <div id="app">
+    <el-config-provider>
+      <TiBtn />
+      <router-view id="router-view" />
+    </el-config-provider>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: "Microsoft YaHei";
+  padding: -10px;
+  background-color: var(--el-bg-color);
+}
+</style>
